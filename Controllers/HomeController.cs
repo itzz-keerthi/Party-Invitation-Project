82c1d6c7c6166c38550ecae301f4e7ce7744e23d
Keerthana J <keerthanajothirameshkumar@gmail.com>
2023-06-30 21:40:11 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() {  
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            //todo: store response from guest
+            Repository.AddResponse(guestResponse);
+            return View("Thanks",guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r=>r.WillAttend==true));
+        }
+    }
+}
